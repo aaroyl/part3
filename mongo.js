1c696065938a2f58fac01cdb5e5cea5b3f6e7bf2
aaroyl <aaro.ylimaula@aalto.fi>
2023-06-07 15:25:23 +0300

    changes

--- mongo.js
+++ mongo.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const mongoose = require('mongoose')
 
 if(process.argv.length < 3){
@@ -18,8 +19,6 @@ const personSchema = new mongoose.Schema({
 })
 
 const Person = mongoose.model('Person', personSchema)
-
-
 
 if(process.argv.length == 3){
     Person.find({}).then(result => {
