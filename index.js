165b417cfe5edc19ae8c8851ade8cbffd24dbd83
aaroyl <aaro.ylimaula@aalto.fi>
2023-06-07 16:17:16 +0300

    changes

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const cors = require('cors')
 const Person = require('./models/person')
 
 app.use(cors())
-app.use(express.json())
 app.use(express.static('build'))
+app.use(express.json())
 
 app.use(morgan(function (tokens, req, res) {
     return [
@@ -20,28 +20,23 @@ app.use(morgan(function (tokens, req, res) {
     ].join(' ')
   }))
 
-let persons = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({error: 'unknown endpoint'})
+}
+
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if(error.name === 'castError'){
+        return response.status(400).send({error: 'malformatted id'})
     }
-]
+
+    next(error)
+}
+
+app.use(errorHandler)
 
 const generateId = () => {
     return Math.floor(Math.random() * 1000000)
@@ -53,17 +48,38 @@ app.get('/api/persons', (request, response) => {
     })
 })
 
-app.get('/api/persons/:id', (request, response) => {
+app.get('/api/persons/:id', (request, response, next) => {
     Person.findById(request.params.id).then(person => {
-        response.json(person)
+        if(person){
+            response.json(person)
+        }else{
+            response.status(404).end()
+        }
     })
+    .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (request, response) => {
+app.delete('/api/persons/:id', (request, response, next) => {
     Person.findByIdAndRemove(request.params.id)
     .then(() => {
         response.status(204).end()
     })
+    .catch(error => next(error))
+})
+
+app.put('/api/persons/:id', (request, response, next) => {
+    const body = request.body
+
+    const person = {
+        name: body.name,
+        number: body.number
+    }
+
+    Person.findByIdAndUpdate(request.params.id, note, {new: true})
+        .then(n => {
+            response.json(n)
+        })
+        .catch(error => next(error))
 })
 
 app.post('/api/persons', (request, response) => {
